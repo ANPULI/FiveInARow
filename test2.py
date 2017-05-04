0a35a94488750e3dccac1c0720552161d8b94276
ANPULI <al4902@nyu.edu>
2017-05-04 16:10:10 +0800

    Bugs fixed.

--- test2.py
+++ test2.py
@@ -3,25 +3,27 @@ import pdb
 
 
 class Five:
-    def __init__(self, maxx, maxy):
-        self.maxx = maxx
-        self.maxy = maxy
+    def __init__(self, max_x, max_y):
+        self.max_x = max_x
+        self.max_y = max_y
         self.board = []
-        for i in range(maxx):
+        for i in range(max_x):
             self.board.append([])
-            for j in range(maxy):
+            for j in range(max_y):
                 self.board[i].append(0)
 
     def start(self):
         '''
-            初始化测试
+            initialization test
         '''
-        # self.qipan[1][1]=self.qipan[2][1]=self.qipan[3][1]=self.qipan[4][1]=self.qipan[5][1]=1
+        # self.board[1][1]=self.board[2][1]=self.board[3][1]=self.board[4][1]=self.board[5][1]=1
         who = False
         os.system('cls')
-        self.printqp()
+        self.printboard()
         while True:
             t = input('Please input(x,y),now is' + ('〇' if who else '●') + ':')
+            if t == "q":  # quit the game
+                break
             t = t.split(',')
             if len(t) == 2:
                 x = int(t[0])
@@ -29,7 +31,7 @@ class Five:
                 if self.board[x][y] == 0:
                     self.board[x][y] = 1 if who else 2
                     os.system('cls')
-                    self.printqp()
+                    self.printboard()
                     ans = self.isWin(x, y)
                     if ans:
                         print(('〇' if who else '●') + 'Wins')
@@ -37,74 +39,69 @@ class Five:
                     who = not who
         os.system('pause')
 
-    def isWin(self, xPoint, yPoint):  # 判赢
+    def isWin(self, xPoint, yPoint):  # determine if one wins
         pdb.set_trace
         flag = False
         t = self.board[xPoint][yPoint]
-        x = xPoint
-        y = yPoint
-        # 横向
-        count = 0
-        x = xPoint
-        y = yPoint
-        while (x > 0 and t == self.board[x][y]):
+
+        # horizontal
+        count, x, y = 0, xPoint, yPoint
+        while x > 0 and t == self.board[x][y]:
             count += 1
             x -= 1
         x = xPoint
-        y = yPoint
-        while (x < self.maxx and t == self.board[x][y]):
+        while x < self.max_x and t == self.board[x][y]:
             count += 1
             x += 1
-        if (count > 5): return True
-        # 纵向
-        count = 0
-        x = xPoint
-        y = yPoint
-        while (y > 0 and t == self.board[x][y]):
+        if count > 5:
+            return True
+
+        # vertical
+        count, x, y = 0, xPoint, yPoint
+        while y > 0 and t == self.board[x][y]:
             count += 1
             y -= 1
         y = yPoint
-        while (y < self.maxy and t == self.board[x][y]):
+        while y < self.max_y and t == self.board[x][y]:
             count += 1
             y += 1
-        if (count > 5): return True
+        if count > 5:
+            return True
+
         # /
-        count = 0
-        x = xPoint
-        y = yPoint
-        while (x > 0 and y < self.maxy and t == self.board[x][y]):
-            count += 1
-            x += 1
-            y -= 1
-        x = xPoint
-        y = yPoint
-        while (x < self.maxx and y > 0 and t == self.board[x][y]):
+        count, x, y = 0, xPoint, yPoint
+        while x > 0 and y < self.max_y and t == self.board[x][y]:
             count += 1
             x -= 1
             y += 1
-        if (count > 5): return True
+        x, y = xPoint, yPoint
+        while x < self.max_x and y > 0 and t == self.board[x][y]:
+            count += 1
+            x += 1
+            y -= 1
+        if count > 5:
+            return True
+
         # \
-        count = 0
-        x = xPoint
-        y = yPoint
-        while (x > 0 and y > 0 and t == self.board[x][y]):
-            count += 1
-            x += 1
-            y -= 1
-        x = xPoint
-        y = yPoint
-        while (x < self.maxx and y < self.maxy and t == self.board[x][y]):
+        count, x, y = 0, xPoint, yPoint
+        while x > 0 and y > 0 and t == self.board[x][y]:
             count += 1
             x -= 1
+            y -= 1
+        x, y = xPoint, yPoint
+        while x < self.max_x and y < self.max_y and t == self.board[x][y]:
+            count += 1
+            x += 1
             y += 1
-        if (count > 5): return True
+        if count > 5:
+            return True
         return False
 
-    def printqp(self):  # 打印棋盘
+    def printboard(self):  # print the board
         print(' 〇一二三四五六七八九')
-        for i in range(self.maxx):
+        for i in range(self.max_x):
             print(i, end='')
-            for j in range(self.maxy):
+            for j in range(self.max_y):
                 if self.board[i][j] == 0:
                     print('十', end='')
                 elif self.board[i][j] == 1:
